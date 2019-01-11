27d0dd959ab12eda393fe6537cb76392d6065f09
adeleon <adeleon@linode.com>
2019-01-10 23:57:13 -0500

    update project card style

--- components/Project.js
+++ components/Project.js
@@ -1,11 +1,11 @@
 export default ({ url, codeUrl, imageSource, caption, description, tags }) => (
   <article role="article">
+    <figure>
+      <img src={imageSource} />
+      <figcaption>{url}</figcaption>
+    </figure>
     <div className="projectInfo">
       <h2>{caption}</h2>
-      <figure>
-        <img src={imageSource} />
-        <figcaption>{url}</figcaption>
-      </figure>
       <p>{description}</p>
       <h3>Technologies</h3>
       <ul className="tags" role="list">
@@ -17,28 +17,25 @@ export default ({ url, codeUrl, imageSource, caption, description, tags }) => (
           )
         })}
       </ul>
-    </div>
-    <div className="externalLinks">
-      <a className="projectLink" href={url}>
-        Live project link
-      </a>
-      <a className="codeLink" href={codeUrl}>
-        Check out the code here
-      </a>
+      <div className="externalLinks">
+        <a className="projectLink" href={url}>
+          Live project link
+        </a>
+        <a className="codeLink" href={codeUrl}>
+          Check out the code here
+        </a>
+      </div>
     </div>
     <style jsx>{`
       article {
         outline: 1px solid;
-        width: 15rem;
+        max-width: 18rem;
         margin: 1rem;
-        padding: 1rem;
-      }
-
-      h2 {
-        text-align: center;
       }
 
       figure {
+        margin: 0;
+        width: 100%;
         text-align: center;
       }
 
@@ -59,6 +56,11 @@ export default ({ url, codeUrl, imageSource, caption, description, tags }) => (
       }
 
       .projectInfo {
+        padding: 0 1rem;
+      }
+
+      h2 {
+        margin-top: 0.5rem;
       }
 
       .externalLinks {
