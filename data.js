6b4a898923c36569a937f4c82e2a6082f92cb4c9
ajdeleon <ajdeleon1064@gmail.com>
2019-05-01 15:04:17 -0400

    add phila reads project

--- data.js
+++ data.js
@@ -1,5 +1,14 @@
 export const projectData = [
   {
+    url: 'http://philareads.herokuapp.com',
+    codeUrl: 'https://github.com/ajdeleon/philareads',
+    imageSource: '/static/philareads.png',
+    caption: 'Philadelphia READS',
+    description:
+      "Philadelphia READS is an open source project aimed at helping to improve literacy in Philadelphia schools through technology that allows students to continue learning in a fun environment outside of regular hours. The project started with a team as a University Project and was passed to a group that I help run: Freecodecamp Philadelphia. Our goal with the project is both to help finish up the app so that it can be piloted with real students in the Philadelphia region, and also to help members of our coding group to make their first contribution to an open-source project in a supportive environment.",
+    tags: ['docker', 'react', 'postgresql', 'flask', 'reactstrap'],
+  },
+  {
     url: 'http://ajdev.xyz',
     codeUrl: 'https://github.com/ajdeleon/indego_project',
     imageSource: '/static/indego.png',
@@ -23,7 +32,7 @@ export const projectData = [
     imageSource: '/static/phitcalendar.png',
     caption: 'Accessible Phit Calendar',
     description:
-      'The calendar for the theater where I do Improv comedy is notoriously inaccessible and complete unresponsive for mobile. This project uses a node server to collect and parse html data, and then a React frontend renders it in a much more readable way.',
+      'The calendar for the theater where I perform Improv comedy is notoriously inaccessible and complete unresponsive for mobile. This project uses a node server to collect and parse html data, and then a React frontend renders it in a much more readable way.',
     tags: ['react', 'nodejs', 'docker', 'nginx', 'cloud hosting', 'tachyons'],
   },
   {
@@ -32,7 +41,7 @@ export const projectData = [
     imageSource: '/static/amex.png',
     caption: 'NYTE Shift Comedy Website',
     description:
-      'This is the website for my improv team NYTE Shift. We very recently had to change our name due to a cease and desist letter and this site will soon be updated to reflect the changes. This was hosted using Netlify.',
+      'This is the website for my improv team NYTE Shift. It uses a very basic React setup for quick and easy development. The site is statically built and hosted using Netlify.',
     tags: ['react', 'cloud hosting'],
   },
   {
